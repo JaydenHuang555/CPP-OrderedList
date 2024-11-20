18c214553a49c58b42596f07602f1d2e65556456
jaydenuang555 <jaydenkanahuang@outlook.com>
2024-11-20 22:12:42 +0000

    rewrote for optimization

--- OrderedList.h
+++ OrderedList.h
@@ -1,49 +1,56 @@
 #pragma once
 
-#include <new>
+#include <ostream>
 #include <stdexcept>
+#include <iostream>
 template<typename T>
 class OrderedList {
-    unsigned long long size, cap;
-    T *subList, tail;
+#define size (((unsigned long)(tail - subList)))
+    unsigned long cap;
+    T *subList, *tail;
 
 public:
     OrderedList(){
         this->cap = 1 << 3;
-        this->size = 0;
         this->subList = new T[this->cap];
         this->tail = subList;
     }
 
     OrderedList(OrderedList<T> &other){
-         this->cap = other->cap;
-         this->size = other->size;
-         this->subList = new T[this->cap];
-         for(int i = 0; i < cap; i++) subList[i] = other.subList[i];
-         this->tail = other.tail;
+        this->cap = other.cap;
+        this->subList = other.subList;
+        this->tail = other.tail;
     }
 
     ~OrderedList(){
         delete[] subList;
     }
-
+    
     void add(T item){
-        subList[size++] = item;
+        *(this->tail++) = item;
         if(size == cap){
             T *next = new T[cap *= 2];
-            for(unsigned long long i = 0; i < cap / 2; i++) next[i] = subList[i];
+            for(int i = 0; i < cap / 2; i++) next[i] = subList[i];
             delete[] subList;
             subList = next;
+            tail = (subList + cap);
         }
     }
 
-    T get(int index){
-        if(index > size || index < 0) throw std::invalid_argument("invalid index given");
+    T get(unsigned long index){
+        if(index > size || index < 0) throw std::invalid_argument("OrderedList: invalid argument given");
         return subList[index];
     }
 
-    bool contains(T item){
-        for(int i = 0; i< size; i++) if(subList[i] == item) return true;
-        return false;
+    T operator[](unsigned long index){
+        return get();
     }
+    
+    friend std::ostream &operator<<(std::ostream &stream, const OrderedList<T> &list){
+        stream << "{ ";
+        for(unsigned long i = 0; i < (unsigned long)(list.tail - list.subList); i++) stream << list.subList[i] << ", ";
+        stream << "\b\b }";
+        return stream;
+    }
+
 };
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "OrderedList.h"
+
+typedef char byte;
+
+int main(int argc, byte **argv){
+    OrderedList<const byte*> *ls = new OrderedList<const byte*>();
+    ls->add("test");
+    ls->add("test3");
+    ls->add("90");
+    std::cout << *ls << "\n";
+    return 0;
+}
